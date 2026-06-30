dc4babd44e463810233f5362142523ee45cdaede
viveek-sh <get.viveksahu@gmail.com>
2026-06-30 12:41:51 +0530

    docs: add leetcode problem link

--- src/Maths/BSSqrt.java
+++ src/Maths/BSSqrt.java
@@ -1,3 +1,7 @@
+//69. Sqrt(x)
+//Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+//You must not use any built-in exponent function or operator.
+//https://leetcode.com/problems/sqrtx/description/
 package Maths;
 
 public class BSSqrt {
