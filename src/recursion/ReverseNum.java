18cce678887f6accb376c4e55396e81ed64d9460
viveek-sh <get.viveksahu@gmail.com>
2026-07-02 16:09:36 +0530

    feat(recursion): add reverse num and check palindrome

---
+++ src/recursion/ReverseNum.java
@@ -0,0 +1,22 @@
+package recursion;
+
+public class ReverseNum {
+    static void main() {
+        int n = 12321;
+        System.out.println(reverse(n,  0));
+        System.out.printf("%d is Palindrome : %s",n,isPalindrome(n));
+    }
+    static int reverse(int n, int rev){
+        if (Math.abs(rev) >= Integer.MAX_VALUE){
+            return 0;
+        }
+        if ( n == 0){
+            return rev;
+        }
+//        System.out.println(rev);
+        return reverse(n / 10, rev * 10 + n % 10);
+    }
+    static boolean isPalindrome(int n){
+        return n == reverse(n, 0);
+    }
+}
