e4c89bfcf628a13165b0b413ae714af01cc09b48
viveek-sh <get.viveksahu@gmail.com>
2026-07-04 12:28:06 +0530

    feat(recursion): feat add new triangle

--- src/recursion/Pattern1.java
+++ src/recursion/Pattern1.java
@@ -3,6 +3,7 @@ package recursion;
 public class Pattern1 {
     static void main() {
         pattern1(5,0);
+        pattern2(5,0);
     }
     static void pattern1(int r, int c){
         if(r == 0){
@@ -15,6 +16,19 @@ public class Pattern1 {
         else {
             System.out.println();
             pattern1(--r, 0);
+        }
+    }
+    static void pattern2(int r, int c){
+        if(r == 0){
+            return;
+        }
+        if(c < r){
+            pattern2(r, ++c);
+            System.out.print("* ");
+        }
+        else {
+            pattern2(--r, 0);
+            System.out.println();
         }
     }
 }
