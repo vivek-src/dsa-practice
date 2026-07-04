06d25f55c79ed6fd54b0d3288ed67c0a9804dd95
viveek-sh <get.viveksahu@gmail.com>
2026-07-04 06:01:04 +0530

    feat(recursion): add find all index of element in array

--- src/recursion/ArraySearcing.java
+++ src/recursion/ArraySearcing.java
@@ -1,10 +1,15 @@
 package recursion;
 
+import java.util.ArrayList;
+
 public class ArraySearcing {
     static void main() {
         int[] arr = {1,2,4,5,6,9,12};
         int ans = findElement(arr, 9, 0);
-        System.out.print(ans);
+        System.out.println(ans);
+
+        int[] arr2 = { 1,2,4,6,6,3,9};
+        System.out.println(findAllIndex(arr2,6,  0, new ArrayList<>()));
 
     }
     static int findElement(int[] arr, int target, int index){
@@ -15,8 +20,13 @@ public class ArraySearcing {
             return index;
         }
         return findElement(arr, target, ++index);
-
-
-
+    }
+    static ArrayList findAllIndex(int[] arr, int target, int index, ArrayList list){
+        if(index > arr.length-1){
+            return list;
+        } else if (arr[index] == target) {
+            list.add(index);
+        }
+        return findAllIndex(arr,target,++index,list);
     }
 }
