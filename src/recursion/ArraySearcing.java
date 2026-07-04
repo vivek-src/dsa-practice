d27f96202e6bba5b06bf0ff96665b7d7325df77c
viveek-sh <get.viveksahu@gmail.com>
2026-07-04 11:32:42 +0530

    feat(recursion): passing result to previous fun calls

--- src/recursion/ArraySearcing.java
+++ src/recursion/ArraySearcing.java
@@ -1,6 +1,7 @@
 package recursion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ArraySearcing {
     static void main() {
@@ -10,7 +11,7 @@ public class ArraySearcing {
 
         int[] arr2 = { 1,2,4,6,6,3,9};
         System.out.println(findAllIndex(arr2,6,  0, new ArrayList<>()));
-
+        System.out.println(findAllIndex2(arr2,6,  0));
     }
     static int findElement(int[] arr, int target, int index){
         if(index > arr.length-1){
@@ -28,5 +29,16 @@ public class ArraySearcing {
             list.add(index);
         }
         return findAllIndex(arr,target,++index,list);
+    }
+    static ArrayList findAllIndex2(int[] arr, int target, int index){
+        ArrayList<Integer> list = new ArrayList<>();
+        if(index > arr.length-1){
+            return list;
+        } else if (arr[index] == target) {
+            list.add(index);
+        }
+
+        list.addAll(findAllIndex2(arr,target,++index));
+        return list;
     }
 }
