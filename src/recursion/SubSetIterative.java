6f1ec7b3d743ab83a94903367e11995d304c6e65
Vivek <get.viveksahu@gmail.com>
2026-08-05 14:32:00 +0530

    add iterative approach to array subset

---
+++ src/recursion/SubSetIterative.java
@@ -0,0 +1,30 @@
+package recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubSetIterative {
+    static void main() {
+        int[] arr = {1,2,3};
+        List<List<Integer>> subset = Subset(arr);
+        for(List<Integer> element : subset){
+            System.out.println(element);
+        }
+
+    }
+    static List<List<Integer>> Subset(int[] arr){
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<>());
+        for(int num : arr){
+            int n = ans.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> internal = new ArrayList<>(ans.get(i));
+                internal.add(num);
+                ans.add(internal);
+            }
+        }
+
+        return ans;
+
+    }
+}
