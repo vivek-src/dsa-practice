b82cb6db72717dd1aae6620e22cd84bd893cf6b7
Vivek <get.viveksahu@gmail.com>
2026-08-10 12:08:00 +0530

    fix(recursion): support 4-letter mappings for digits 7 and 9

--- src/recursion/LetterCombination.java
+++ src/recursion/LetterCombination.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class LetterCombination {
     static void main() {
-        ArrayList<String> ans = getCombination("", "2");
+        ArrayList<String> ans = getCombination("", "8");
         System.out.println(ans);
 
     }
@@ -18,8 +18,15 @@ public class LetterCombination {
         }
         char digit = up.charAt(0);
         ArrayList<String> ans = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            char letter = (char) ('a' + (digit - '2') * 3 + i);
+        int start = (digit - '2') * 3;
+
+        if (digit > '7') {
+            start++;
+        }
+
+        int end = (digit == '7' || digit == '9') ? 4 : 3;
+        for (int i = 0; i < end; i++) {
+            char letter = (char) ('a' + start + i);
             ArrayList<String> result = getCombination(p + letter, up.substring(1));
             ans.addAll(result);
         }
--- src/recursion/Permutations.java
+++ src/recursion/Permutations.java
@@ -1,10 +1,8 @@
 package recursion;
 
-import java.util.ArrayList;
 public class Permutations {
     static void main() {
         printPermutations("", "abc");
-        System.out.println(Permutation("", "abc"));
 
     }
     static void printPermutations(String p, String up){
@@ -18,22 +16,5 @@ public class Permutations {
             String second = p.substring(i);
             printPermutations(first + ch + second, up.substring(1));
         }
-    }
-
-    static ArrayList<String> Permutation(String p, String up){
-        if (up.isEmpty()){
-            ArrayList<String> list = new ArrayList<>();
-            list.add(p);
-            return list;
-        }
-        char ch = up.charAt(0);
-
-        ArrayList<String> ans = new ArrayList<>();
-        for (int i = 0; i <= p.length(); i++) {
-            String first = p.substring(0,i);
-            String second = p.substring(i);
-            ans.addAll(Permutation(first + ch + second, up.substring(1)));
-        }
-        return  ans;
     }
 }
