f899776d23901bc7d852ab9d3d4a45e57102b60e
Vivek <get.viveksahu@gmail.com>
2026-08-11 14:35:00 +0530

    feat(recursion): add ArrayList return for char combo in NumPad

--- src/recursion/LetterCombination2.java
+++ src/recursion/LetterCombination2.java
@@ -1,9 +1,13 @@
 //  Same Approach written better
 package recursion;
 
+import java.util.ArrayList;
+
 public class LetterCombination2 {
     static void main() {
-        numPad("","12");
+//        numPad("","12");
+        ArrayList<String> charCombo = numPadCombo("", "12");
+        System.out.println(charCombo);
     }
     static void numPad(String p, String up){
         if(up.isEmpty()){
@@ -15,6 +19,20 @@ public class LetterCombination2 {
             char ch = (char) ('a' + i);
             numPad(p + ch, up.substring(1));
         }
-
+    }
+    static ArrayList<String> numPadCombo(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> ans = new ArrayList<>();
+            ans.add(p);
+            return ans;
+        }
+        int digit = (char) up.charAt(0) - '0';
+        ArrayList<String> ans = new ArrayList<>();
+        for (int i = (digit - 1 )*3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            ArrayList<String> branch = numPadCombo(p + ch, up.substring(1));
+            ans.addAll(branch);
+        }
+        return ans;
     }
 }
