7b3bd28e54383fa15c9e9c7fe2fd48c24e39939c
Vivek <get.viveksahu@gmail.com>
2026-08-14 17:42:00 +0530

    feat(recursion): add dice throw combination algorithms

---
+++ src/recursion/DicethrowCombo.java
@@ -0,0 +1,28 @@
+package recursion;
+
+public class DicethrowCombo {
+    static void main() {
+//        getCombo("",4);
+        System.out.println(getNoRolls("", 6, 3));
+
+    }
+    static void getCombo(String p, int target){
+        if( target == 0){
+            System.out.println(p);
+            return;
+        }
+        for (int i = 1; i <=6 && i <= target; i++) {
+            getCombo(p + i, target - i);
+        }
+    }
+    static int getNoRolls(String p, int side,int target){
+        if(target == 0){
+            return 1;
+        }
+        int ans = 0;
+        for (int i = 1; i < side && i <= target; i ++){
+            ans = getNoRolls(p + i, side, target - i);
+        }
+        return ans;
+    }
+}
