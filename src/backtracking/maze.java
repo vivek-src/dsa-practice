056d0e6580354bf1927e59956d6d4a1e383e7295
Vivek <get.viveksahu@gmail.com>
2026-08-15 16:26:00 +0530

    feat(backtracking): count unique paths in grid using recursion

--- src/backtracking/maze.java
+++ src/backtracking/maze.java
@@ -1,4 +1,18 @@
 package backtracking;
 
 public class maze {
+    static void main() {
+        System.out.println(noOfPaths(3,3));
+
+    }
+    static int noOfPaths(int r,int c){
+        if(r == 1 || c == 1){
+            return 1;
+        }
+        int left = noOfPaths(r -1 , c);
+        int right = noOfPaths(r, c - 1);
+
+        return left + right;
+
+    }
 }
