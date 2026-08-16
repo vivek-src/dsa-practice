2583aa5c2880a463fcc548473e90c1c21d9aeb7e
Vivek <get.viveksahu@gmail.com>
2026-08-16 15:20:00 +0530

    feat(backtracking): add ArrayList to Return Paths in the  Grid

--- src/backtracking/maze.java
+++ src/backtracking/maze.java
@@ -1,9 +1,12 @@
 package backtracking;
 
+import java.util.ArrayList;
+
 public class maze {
     static void main() {
 //        System.out.println(noOfPaths(3,3));
-        printPaths("",3,3);
+//        printPaths("",3,3);
+        System.out.println(getPaths("", 3,3));
     }
     static int noOfPaths(int r,int c){
         if(r == 1 || c == 1){
@@ -25,5 +28,20 @@ public class maze {
         if(r > 1){
             printPaths(p+"D", r-1, c);
         }
+    }
+    static ArrayList<String> getPaths(String p, int r, int c){
+        if (r == 1 && c == 1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> Ans = new ArrayList<>();
+        if(c > 1){
+            Ans.addAll(getPaths(p+"R", r, c-1));
+        }
+        if(r > 1){
+            Ans.addAll(getPaths(p+"D", r-1, c));
+        }
+        return Ans;
     }
 }
