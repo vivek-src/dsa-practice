6b96777019f707e4278bd32fa219702438bbdea2
Vivek <get.viveksahu@gmail.com>
2026-08-19 17:18:00 +0530

    feat(backtracking): Add path step tracking to grid traversal

--- src/backtracking/AllDirTraversing.java
+++ src/backtracking/AllDirTraversing.java
@@ -1,5 +1,7 @@
 package backtracking;
 
+import java.util.Arrays;
+
 public class AllDirTraversing {
     static void main() {
         boolean[][] grid = {
@@ -7,7 +9,9 @@ public class AllDirTraversing {
                 {true, true, true},
                 {true, true, true}
         };
-        printPath("", grid, 0,0 );
+        int[][] path  = new int[grid.length][grid[0].length];
+//        printPath("", grid, 0,0 );
+        printGrid("",grid,0,0, path, 0);
 
     }
     static void printPath(String p, boolean[][] grid, int r, int c){
@@ -38,6 +42,40 @@ public class AllDirTraversing {
         }
         //Restore cell for next call
         grid[r][c] = true;
+
+    }
+    static void printGrid(String p, boolean[][] grid, int r, int c, int[][] path, int step){
+        // Termination target is reached
+        if (r == grid.length-1 && c == grid[0].length-1){
+            System.out.println(p);
+            for (int[] row : path){
+                System.out.println(Arrays.toString(row));
+            }
+            System.out.println();
+            return;
+        }else if(!grid[r][c]){         // Invalid/visited cell
+            return;
+        }
+        grid[r][c] = false;
+        path[r][c] = step;
+
+        if (c < grid[0].length-1 ){
+            printGrid(p + "R", grid, r, c+1, path, step+1);
+        }
+
+        if(r < grid.length - 1){
+            printGrid(p + "D", grid, r+1, c, path, step+1);
+        }
+
+        if (c > 0){
+            printGrid(p + "L", grid, r, c-1, path, step+1);
+        }
+
+        if (r > 0){
+            printGrid(p + "U", grid, r-1, c, path, step+1);
+        }
+        grid[r][c] = true;
+        path[r][c] = 0;
 
     }
 }
