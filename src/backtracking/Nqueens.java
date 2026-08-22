4f5a5c44e1a2d9bbb1c40ce2dacdb6f6021d234e
Vivek <get.viveksahu@gmail.com>
2026-08-22 14:36:00 +0530

    refactor(backtracking): update N-Queens to return formatted list of strings

--- src/backtracking/Nqueens.java
+++ src/backtracking/Nqueens.java
@@ -1,11 +1,18 @@
+//https://leetcode.com/problems/n-queens/description/
+// 51. N-Queens
 package backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Nqueens {
     static void main() {
         int n = 4;
         boolean[][] board = new boolean[n][n];
-        placeQueens(board, 0);
-
+//        placeQueens(board, 0);
+        List<List<String>> result = new ArrayList<>();
+        queensSol(board, 0, result);
+        System.out.println(result);
     }
     static void placeQueens(boolean[][] board, int row){
         if (row == board.length) {
@@ -48,5 +55,30 @@ public class Nqueens {
             }
             System.out.println();
         }
+    }
+    static void queensSol(boolean[][] board, int row, List<List<String>> soloution){
+        if(row == board.length){
+            soloution.add(formatSol(board));
+            return;
+        }
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)){
+                board[row][col] = true;
+                queensSol(board, row+1, soloution);
+                board[row][col] = false;
+            }
+        }
+    }
+    static List<String> formatSol(boolean[][] board){
+        List<String> ans = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            StringBuilder rowAns = new StringBuilder();
+            for (int j = 0; j < board[i].length ; j++) {
+                if(board[i][j]) rowAns.append("Q");
+                else rowAns.append(".");
+            }
+            ans.add(rowAns.toString());
+        }
+        return ans;
     }
 }
