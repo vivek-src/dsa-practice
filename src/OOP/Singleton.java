0916fb1caaa19d4adac2b2dcefb0084a090124e5
Vivek <get.viveksahu@gmail.com>
2026-09-01 13:08:57 +0530

    feat(OOP): revisit singleton class in Java

--- src/OOP/Singleton.java
+++ src/OOP/Singleton.java
@@ -1,10 +1,34 @@
 package OOP;
 
-public class Singelton {
+public class Singleton {
 
 
     static void main() {
-        
+//        DBInstance mysql = new DBInstance();
+        DBInstance mysql = DBInstance.getInstance();
+        DBInstance pgsql = DBInstance.getInstance();
 
+        // weather both the instances are same
+        System.out.println(mysql == pgsql);
+    }
+}
+
+class DBInstance{
+    private DBInstance(){
+        connectToDatabase();
+    };
+
+    private static DBInstance instance;
+
+    static DBInstance getInstance(){
+        if (instance == null){
+            instance = new DBInstance();
+        }
+        return instance;
+    }
+
+    private void connectToDatabase() {
+        // db connection logic here
+        System.out.println("Connecting to DB...");
     }
 }
